165e815c4c6af71aec948b6adae983c540e93c49
Haruo <haruo-mtok@outlook.com>
2019-11-18 00:06:00 +0900

    Add main

---
+++ src/utility.zsh
@@ -0,0 +1,20 @@
+alias v='$VISUAL'
+alias e='$EDITOR'
+alias p='$PAGER'
+
+alias ls='ls --group-directories-first --color=auto -Xv'
+alias l='ls -1A'         # Lists in one column, hidden files.
+alias ll='ls -lh'        # Lists human readable sizes.
+alias la='ll -A'         # Lists human readable sizes, hidden files.
+alias tree='find . -type d | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"'
+
+# Makes a directory and changes to it.
+function mkdcd {
+  [[ -n "$1" ]] && mkdir -p "$1" && builtin cd "$1"
+}
+
+# Changes to a directory and lists its contents.
+function cdls {
+  builtin cd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+alias cd='cdls'

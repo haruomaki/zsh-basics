be7ec47f04ad8b4997394c8dd231f29d11510650
Haruo <haruo-mtok@outlook.com>
2020-04-18 17:44:26 +0900

    Change history file
    書式がzshとbashで異なるため、bashから正常に参照できない。また、bash側の保存数上限によって意図せずに履歴が消えてしまう。

--- src/history.zsh
+++ src/history.zsh
@@ -9,7 +9,7 @@ setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing non-existent history.
 
-HISTFILE="${HOME}/.bash_history"  # The path to the history file.
+HISTFILE="${HOME}/.zsh_history"  # The path to the history file.
 HISTSIZE=10000                   # The maximum number of events to save in the internal history.
 SAVEHIST=10000                   # The maximum number of events to save in the history file.
 

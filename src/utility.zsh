6d874d5f1f05faf6c0fe4fe2727c7c948171f590
Haruo <mtok-haruo@outlook.com>
2020-12-31 14:10:55 +0900

    Fix cdls

--- src/utility.zsh
+++ src/utility.zsh
@@ -5,12 +5,18 @@ alias b='$BROWSER'
 
 type exa >/dev/null 2>&1
 if [ $? -eq 0 ]; then
+  function newls {
+    exa --sort=type --git
+  }
   alias ls='exa --sort=type --git'
   alias l='ls -1a'
   alias ll='ls -l'
   alias la='ll -a'
-  alias tree='ls --tree'
+  alias tree='ls -l --tree'
 else
+  function newls {
+    ls --group-directories-first --color=auto -Xv
+  }
   alias ls='ls --group-directories-first --color=auto -Xv'
   alias l='ls -1A'         # Lists in one column, hidden files.
   alias ll='ls -lh'        # Lists human readable sizes.
@@ -25,6 +31,6 @@ function mkdcd {
 
 # Changes to a directory and lists its contents.
 function cdls {
-  builtin cd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+  builtin cd "$argv[-1]" && newls "${(@)argv[1,-2]}"
 }
 alias cd='cdls'

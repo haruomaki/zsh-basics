33b4932b25cad40ff9773f47fb1e44897925b521
Haruo <mtok-haruo@outlook.com>
2020-12-31 14:02:42 +0900

    Add exa

--- src/utility.zsh
+++ src/utility.zsh
@@ -3,11 +3,20 @@ alias e='$EDITOR'
 alias p='$PAGER'
 alias b='$BROWSER'
 
-alias ls='ls --group-directories-first --color=auto -Xv'
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias tree='find . -type d | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"'
+type exa >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  alias ls='exa --sort=type --git'
+  alias l='ls -1a'
+  alias ll='ls -l'
+  alias la='ll -a'
+  alias tree='ls --tree'
+else
+  alias ls='ls --group-directories-first --color=auto -Xv'
+  alias l='ls -1A'         # Lists in one column, hidden files.
+  alias ll='ls -lh'        # Lists human readable sizes.
+  alias la='ll -A'         # Lists human readable sizes, hidden files.
+  alias tree='find . -type d | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"'
+fi
 
 # Makes a directory and changes to it.
 function mkdcd {

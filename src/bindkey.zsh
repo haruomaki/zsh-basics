9b503a3a7390f956a374c6056a57441103609f37
Haruo <mtok-haruo@outlook.com>
2021-02-19 22:30:27 +0900

    add bindkeys

--- src/bindkey.zsh
+++ src/bindkey.zsh
@@ -4,3 +4,11 @@ bindkey -e
 # Use ctrl+arrow to move word by word
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
+
+# Use Home/End 
+bindkey -s "^[[H" "^a"
+bindkey -s "^[[F" "^e"
+
+# history-search
+bindkey "^[[A" history-search-backward
+bindkey "^[[B" history-search-forward
